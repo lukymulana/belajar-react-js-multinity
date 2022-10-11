388c37cbaea126eef7321b2124860b331177e9ec
Luky Mulana <lukymulana@gmail.com>
2022-10-11 15:39:50 +0700

    Conditional Rendering Bag 3

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
 const root = document.querySelector('#root')
 
 function App() {
-   const [login, setLogin] = React.useState(false);
-
-   if (login) {
-      return (
-         <>
-            <h1>Success</h1>
-            <button onClick={function () {
-               setLogin(false);
-            }}>
-               Logout
-            </button>
-         </>
-      )
-   }   
+   const [login, setLogin] = React.useState(false);  
    
    return (
       <>
          <h1>Klik Login !!!</h1>
+         <p>{login ? <b>Success</b> : <i>Error</i>}</p>
          <button onClick={function () {
             setLogin(true);
          }}>
